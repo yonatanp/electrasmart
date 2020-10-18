de82f508e12304dad1f73c0e6d1c1de8caa341a5
Yonatan Perry <yonatan.perry@gmail.com>
2020-10-18 12:46:37 +0300

    cli: add `electrasmart-send-command` to send ac commands from the cli

--- electrasmart/cli.py
+++ electrasmart/cli.py
@@ -34,7 +34,7 @@ def list_devices():
 
 
 def gen_baseline_status():
-    parser = ArgumentParser("Auth token generator")
+    parser = ArgumentParser("Generate baseline status")
     _add_ac_arguments(parser)
     parser.add_argument("output_file", default=None, help="target where baseline status is saved (default: baseline_status_<ac_id>.json)")
     args = parser.parse_args()
@@ -47,6 +47,29 @@ def gen_baseline_status():
     json.dump(status, f)
     print(f"Baseline status for AC id {args.ac_id} written successfully to "
           f"{args.output_file if args.output_file != '-' else 'stdout'}")
+
+
+def send_command():
+    parser = ArgumentParser("Send command to the AC unit")
+    _add_ac_arguments(parser)
+    parser.add_argument("--ac-mode", default=None, help="set the AC mode",
+                        choices=["STBY", "COOL", "FAN", "HEAT", "DRY"])  # TODO: add and verify choices
+    parser.add_argument("--fan-speed", default=None, help="set the fan speed",
+                        choices=["LOW", "MEDIUM", "HIGH", "AUTO"])
+    parser.add_argument("--temperature", default=None, type=int, help="set the target temperature")
+    args = parser.parse_args()
+    oper_kwargs = {}
+    if args.ac_mode is not None:
+        oper_kwargs['ac_mode'] = args.ac_mode
+    if args.fan_speed is not None:
+        oper_kwargs['fan_speed'] = args.fan_speed
+    if args.temperature is not None:
+        oper_kwargs['temperature'] = args.temperature
+    if not oper_kwargs:
+        parser.exit(message="no change was requested, aborting")
+    ac = AC(args.imei, args.token, args.ac_id)
+    ac.renew_sid()
+    ac.modify_oper(**oper_kwargs)
 
 
 def _add_ac_arguments(parser):
